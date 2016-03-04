71e3c473ae217ddbe3ade1533a5388e1f8ce46a3
68730 <68730@LCNC09V0019.corp.lgcns.com>
2016-03-05 00:12:35 +0900

    add SlowQueryPack

---
+++ scouter.common/src/scouter/lang/pack/SlowQueryPack.java
@@ -0,0 +1,57 @@
+package scouter.lang.pack;
+
+import java.io.IOException;
+
+import scouter.io.DataInputX;
+import scouter.io.DataOutputX;
+
+public class SlowQueryPack implements Pack {
+	public String startTime;
+	public String user;
+	public String host;
+	public String threadId;
+	public String db;
+	public boolean qcHit;
+	public String queryTime;
+	public String lockTime;
+	public String rowsSent;
+	public String rowsExamined;
+	public int sqlHash;
+	
+	@Override
+	public byte getPackType() {
+		return PackEnum.SLOW_QUERY;
+	}
+
+	@Override
+	public void write(DataOutputX out) throws IOException {
+		out.writeText(startTime);
+		out.writeText(user);
+		out.writeText(host);
+		out.writeText(threadId);
+		out.writeText(db);
+		out.writeBoolean(qcHit);
+		out.writeText(queryTime);
+		out.writeText(lockTime);
+		out.writeText(rowsSent);
+		out.writeText(rowsExamined);
+		out.writeInt(sqlHash);
+	}
+
+	@Override
+	public Pack read(DataInputX in) throws IOException {
+		this.startTime = in.readText();
+		this.user = in.readText();
+		this.host = in.readText();
+		this.threadId = in.readText();
+		this.db = in.readText();
+		this.qcHit = in.readBoolean();
+		this.queryTime = in.readText();
+		this.lockTime = in.readText();
+		this.rowsSent = in.readText();
+		this.rowsExamined = in.readText();
+		this.sqlHash = in.readInt();
+		return this;
+	}
+
+}
